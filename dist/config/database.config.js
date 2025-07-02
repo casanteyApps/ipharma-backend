239c7b4d35baef478b655249f86d8f616bb42182
root <root@ipharmasrv.cssmetaverse.com>
2025-07-02 18:48:15 +0000

    fix: db

--- dist/config/database.config.js
+++ dist/config/database.config.js
@@ -3,11 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.getDatabaseConfig = void 0;
 const getDatabaseConfig = (configService) => ({
     type: 'postgres',
-    host: configService.get('DB_HOST', '10.19.32.141'),
-    port: configService.get('DB_PORT', 5432),
-    username: configService.get('DB_USERNAME', 'ipharmadbmaster'),
-    password: configService.get('DB_PASSWORD', 'ZAZAN2620*$'),
-    database: configService.get('DB_NAME', 'ipharma_stg'),
+    host: configService.get('DB_HOST'),
+    port: configService.get('DB_PORT'),
+    username: configService.get('DB_USERNAME'),
+    password: configService.get('DB_PASSWORD'),
+    database: configService.get('DB_NAME'),
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
     synchronize: configService.get('NODE_ENV') !== 'production',
     logging: configService.get('NODE_ENV') === 'development',
