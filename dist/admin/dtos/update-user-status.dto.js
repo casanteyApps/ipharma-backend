550fd8ee2d7dc7d86aa6e627e6ac377435beeb96
AppsAdmin <118912356+casanteyApps@users.noreply.github.com>
2025-08-12 09:21:15 +0000

    Deleted main.js.map

--- dist/admin/dtos/update-user-status.dto.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=update-user-status.dto.js.map
--- dist/suppliers/supplier.dto.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class UpdateInventoryItemDto {
-    productId: string;
-    quantity: number;
-    price: number;
-}
