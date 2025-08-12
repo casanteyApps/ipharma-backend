58d3fc2f63e3024e969b440f902782c8f11982c6
AppsAdmin <118912356+casanteyApps@users.noreply.github.com>
2025-08-12 10:25:15 +0000

    Deleted app.controller.d.ts

--- dist/entities/transaction.entity.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Transaction {
-    id: number;
-    buyer_id: string;
-    seller_id: string;
-    product_id: string;
-    quantity: number;
-    total_cost: number;
-    date: string;
-}
